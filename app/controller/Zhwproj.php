7d96b67f9a2195dbb446cfbe0be898d2a5b65b88
Lijiming82 <25774325@qq.com>
2024-08-15 15:40:45 +0800

    modified:   app/controller/Zhwproj.php

--- app/controller/Zhwproj.php
+++ app/controller/Zhwproj.php
@@ -178,28 +178,28 @@ class Zhwproj
         //输入角度（朝向的角度），给出排列数字BCDEFGHI
 
         if($degree>=337.5||$degree<=22.5){
-            $redata=array(['B'=>1,'C'=>8,'D'=>3,'E'=>4,'F'=>9,'G'=>2,'H'=>7,'I'=>6]);
+            $redata=array('B'=>1,'C'=>8,'D'=>3,'E'=>4,'F'=>9,'G'=>2,'H'=>7,'I'=>6);
             return $redata;//北，1
         }elseif($degree>22.5&&$degree<67.5){
-            $redata=array(['B'=>8,'C'=>3,'D'=>4,'E'=>9,'F'=>2,'G'=>7,'H'=>6,'I'=>1]);
+            $redata=array('B'=>8,'C'=>3,'D'=>4,'E'=>9,'F'=>2,'G'=>7,'H'=>6,'I'=>1);
             return $redata;//东北 8
         }elseif($degree>=67.5&&$degree<=112.5){
-            $redata=array(['B'=>3,'C'=>4,'D'=>9,'E'=>2,'F'=>7,'G'=>6,'H'=>1,'I'=>8]);
+            $redata=array('B'=>3,'C'=>4,'D'=>9,'E'=>2,'F'=>7,'G'=>6,'H'=>1,'I'=>8);
             return $redata;//东 3
         }elseif($degree>112.5&&$degree<157.5){
-            $redata=array(['B'=>4,'C'=>9,'D'=>2,'E'=>7,'F'=>6,'G'=>1,'H'=>8,'I'=>8]);
+            $redata=array('B'=>4,'C'=>9,'D'=>2,'E'=>7,'F'=>6,'G'=>1,'H'=>8,'I'=>8);
             return $redata;//东南 4
         }elseif($degree>=157.5&&$degree<=202.5){
-            $redata=array(['B'=>9,'C'=>2,'D'=>7,'E'=>6,'F'=>1,'G'=>8,'H'=>3,'I'=>4]);
+            $redata=array('B'=>9,'C'=>2,'D'=>7,'E'=>6,'F'=>1,'G'=>8,'H'=>3,'I'=>4);
             return $redata;//南 9
         }elseif($degree>202.5&&$degree<247.5){
-            $redata=array(['B'=>2,'C'=>7,'D'=>6,'E'=>1,'F'=>8,'G'=>3,'H'=>4,'I'=>9]);
+            $redata=array('B'=>2,'C'=>7,'D'=>6,'E'=>1,'F'=>8,'G'=>3,'H'=>4,'I'=>9);
             return $redata;//西南 2
         }elseif($degree>=247.5&&$degree<=292.5){
-            $redata=array(['B'=>7,'C'=>6,'D'=>1,'E'=>8,'F'=>3,'G'=>4,'H'=>9,'I'=>2]);
+            $redata=array('B'=>7,'C'=>6,'D'=>1,'E'=>8,'F'=>3,'G'=>4,'H'=>9,'I'=>2);
             return $redata;//西 7
         }elseif($degree>292.5&&$degree<337.5){
-            $redata=array(['B'=>6,'C'=>1,'D'=>8,'E'=>3,'F'=>4,'G'=>9,'H'=>2,'I'=>7]);
+            $redata=array('B'=>6,'C'=>1,'D'=>8,'E'=>3,'F'=>4,'G'=>9,'H'=>2,'I'=>7);
             return $redata;//西北 6
         }
     }
